b7009dc513b72021851a8f797e6c7676d24f24e7
prabinpanta0 <pantaprabin30@gmail.com>
2025-04-10 21:51:53 +0545

    Add example demonstrating string manipulation and type conversion in Lua

---
+++ 20.lua
@@ -0,0 +1,31 @@
+-- string variable
+str = "10"
+
+-- print type of string variable
+print(type(str))
+
+--double the number
+num = 2 * str
+
+-- print the number
+print(num)
+
+-- print the type of number variable
+print(type(num))
+
+-- uncommon method
+-- add 0 to string
+num = str + 0
+print(type(num))
+
+-- print the num variable
+print(num)
+
+-- uncommon method (using a libary function)
+-- convert str to numver
+num = tonumber(str)
+
+-- print type of num variable as number
+print(type(num))
+-- print value of num variable
+print(num)
