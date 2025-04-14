991011ef133a378aed95eef24ee79e50b863c019
prabinpanta0 <pantaprabin30@gmail.com>
2025-04-14 17:14:39 +0545

    Add search function using a set for improved fruit lookup

--- 31.lua
+++ 31.lua
@@ -33,3 +33,23 @@ target2 = "mango"
 
 searchFruit(fruits, target1)
 searchFruit(fruits, target2)
+
+-- using a set
+local fruitsSet = {
+    apple = true,
+    bannana = true,
+    orange = true,
+    ["1"] = true,
+    ["2"] = true
+}
+-- Search function using a set
+function searchFruitSet(fruitsSet, target)
+    if fruitsSet[target] then
+        print("Item Found")
+    else
+        print("Item not found")
+    end
+end
+
+searchFruitSet(fruitsSet, target1)
+searchFruitSet(fruitsSet, target2)
